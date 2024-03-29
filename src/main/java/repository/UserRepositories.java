994c069c66499e142c11995c6297c2e560dc45e8
C0823G1-nguyentrucvi <nguyentrucvi10112004@gmail.com>
2024-03-29 09:00:44 +0700

    vi fix bug

--- src/main/java/repository/UserRepositories.java
+++ src/main/java/repository/UserRepositories.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import DB.DBConnection;
 import model.User;
 import repository.IUserRepositories;
 
@@ -8,37 +9,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepositories implements IUserRepositories {
-    private String jdbcURL = "jdbc:mysql://localhost:3306/user?useSSL=false";
-    private String jdbcUsername = "root";
-    private String jdbcPassword = "123456";
+//    private String jdbcURL = "jdbc:mysql://localhost:3306/user?useSSL=false";
+//    private String jdbcUsername = "root";
+//    private String jdbcPassword = "123456";
 
-    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, number, country) VALUES (?, ?, ?);";
-    private static final String SELECT_USER_BY_ID = "select id,name,number,country from users where id =?";
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, phone_number, address) VALUES (?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "select id,name,phone_number,address from users where id =?";
     private static final String SELECT_ALL_USERS = "select * from users";
     private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
-    private static final String UPDATE_USERS_SQL = "update users set name = ?,number= ?, country =? where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,phone_number= ?, address =? where id = ?;";
 
-    protected Connection getConnection(){
-        Connection connection = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(jdbcURL,jdbcUsername,jdbcPassword);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
-    }
+//    protected Connection getConnection(){
+//        Connection connection = null;
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            connection = DriverManager.getConnection(jdbcURL,jdbcUsername,jdbcPassword);
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return connection;
+//    }
 
     @Override
     public void insertUser(User user) throws SQLException {
         System.out.println(INSERT_USERS_SQL);
-        try (Connection connection = getConnection();
+        try (Connection connection = DBConnection.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
             preparedStatement.setString(1, user.getName());
-            preparedStatement.setInt(2, user.getNumber());
-            preparedStatement.setString(3, user.getCountry());
+            preparedStatement.setInt(2, user.getPhone_number());
+            preparedStatement.setString(3, user.getAddress());
             System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
         }catch (SQLException e){
@@ -49,7 +50,7 @@ public class UserRepositories implements IUserRepositories {
     @Override
     public User selectUser(int id) {
         User user = null;
-        Connection connection = getConnection();
+        Connection connection =DBConnection.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);
             preparedStatement.setInt(1,id);
@@ -57,8 +58,8 @@ public class UserRepositories implements IUserRepositories {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
                 String name = resultSet.getString("name");
-                int number = resultSet.getInt("number");
-                String country = resultSet.getString("country");
+                int number = resultSet.getInt("phone_number");
+                String country = resultSet.getString("address");
                 user = new User(id,name,number,country);
             }
         } catch (SQLException e) {
@@ -70,7 +71,7 @@ public class UserRepositories implements IUserRepositories {
     @Override
     public List<User> selectAllUsers() {
         List<User> users = new ArrayList<>();
-        Connection connection = getConnection();
+        Connection connection =DBConnection.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);
             System.out.println(preparedStatement);
@@ -78,8 +79,8 @@ public class UserRepositories implements IUserRepositories {
             while (rs.next()){
                 int id = rs.getInt("id");
                 String name = rs.getString("name");
-                int number = rs.getInt("number");
-                String country = rs.getString("country");
+                int number = rs.getInt("phone_number");
+                String country = rs.getString("address");
                 users.add(new User(id,name,number,country));
             }
         } catch (SQLException e) {
@@ -91,7 +92,7 @@ public class UserRepositories implements IUserRepositories {
     @Override
     public boolean deleteUser(int id) throws SQLException {
         boolean rowDeleted;
-        try(Connection connection = getConnection();
+        try(Connection connection =DBConnection.getConnection();
             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_USERS_SQL);) {
             preparedStatement.setInt(1, id);
             rowDeleted = preparedStatement.executeUpdate() > 0;
@@ -102,11 +103,11 @@ public class UserRepositories implements IUserRepositories {
     @Override
     public boolean updateUser(User user) throws SQLException {
         boolean rowUpdated;
-        Connection connection = getConnection();
+        Connection connection =DBConnection.getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_USERS_SQL);
         preparedStatement.setString(1,user.getName());
-        preparedStatement.setInt(2,user.getNumber());
-        preparedStatement.setString(3,user.getCountry());
+        preparedStatement.setInt(2,user.getPhone_number());
+        preparedStatement.setString(3,user.getAddress());
         preparedStatement.setInt(4,user.getId());
         rowUpdated = preparedStatement.executeUpdate() > 0;
         return rowUpdated ;
